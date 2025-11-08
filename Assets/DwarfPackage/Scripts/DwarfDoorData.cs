b772aaa05ee62cc1593f75826311bcf7a61bc97c
rodarg877 <50242480+rodarg877@users.noreply.github.com>
2025-11-08 17:46:31 -0300

    a

--- Assets/DwarfPackage/Scripts/DwarfDoorData.cs
+++ Assets/DwarfPackage/Scripts/DwarfDoorData.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using UnityEngine;
 
 namespace SkyrimProject.DwarvenPackage
@@ -14,4 +15,22 @@ namespace SkyrimProject.DwarvenPackage
         [HideInInspector] public Quaternion closedRotation;
         [HideInInspector] public Quaternion openRotation;
     }
+=======
+using UnityEngine;
+
+namespace SkyrimProject.DwarvenPackage
+{
+    public class DwarfDoorData : MonoBehaviour
+    {
+        public Transform hingePoint;      // Punto de bisagra (Empty a un costado)
+        public float openAngle = 90f;     // Ángulo de apertura
+        public float speed = 2f;          // Velocidad de rotación
+        public bool invertDirection = false; // Para abrir hacia el otro lado
+
+        [HideInInspector] public bool isOpen = false;
+        [HideInInspector] public Coroutine coroutine;
+        [HideInInspector] public Quaternion closedRotation;
+        [HideInInspector] public Quaternion openRotation;
+    }
+>>>>>>> 83b979d6c08304e2436793e39b939edeec89c92c
 }
